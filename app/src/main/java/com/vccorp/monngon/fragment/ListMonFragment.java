e13b2485eb42a12addbd2b01131ad2380d6038d3
thaond <thao.nd.276@gmail.com>
2016-11-17 18:09:15 +0700

    no message

---
+++ app/src/main/java/com/vccorp/monngon/fragment/ListMonFragment.java
@@ -0,0 +1,8 @@
+//package com.vccorp.monngon.fragment;
+//
+///**
+// * Created by PC0353 on 11/17/2016.
+// */
+//
+//public class ListMonFragment extends  {
+//}
--- app/src/main/java/com/vccorp/monngon/fragment/TodayMenuFragment.java
+++ app/src/main/java/com/vccorp/monngon/fragment/TodayMenuFragment.java
@@ -1,6 +1,8 @@
 package com.vccorp.monngon.fragment;
 
-import android.app.Activity;
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -11,7 +13,9 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.thaond.library.util.Utils;
+import com.vccorp.monngon.MainActivity;
 import com.vccorp.monngon.R;
+import com.vccorp.monngon.activity.DetailMonActivity;
 import com.vccorp.monngon.model.Mon;
 import com.vccorp.monngon.util.Constants;
 
@@ -24,11 +28,13 @@ public class TodayMenuFragment extends Fragment {
     private View mView;
     private TextView txtTitle;
     private ImageView imageMenu;
-    private Activity activity;
+    private MainActivity activity;
+    private Mon mon;
+
     public static TodayMenuFragment newInstance(Mon mon) {
         TodayMenuFragment myFragment = new TodayMenuFragment();
-        Bundle bundle=new Bundle();
-        bundle.putParcelable(Constants.DATA,mon);
+        Bundle bundle = new Bundle();
+        bundle.putParcelable(Constants.DATA, mon);
         myFragment.setArguments(bundle);
         return myFragment;
     }
@@ -36,19 +42,36 @@ public class TodayMenuFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        mView=inflater.inflate(R.layout.fragment_today_menu,container,false);
-        activity=getActivity();
-        txtTitle=(TextView)mView.findViewById(R.id.txt_title_menu);
-        imageMenu=(ImageView)mView.findViewById(R.id.image_today_menu);
+        mView = inflater.inflate(R.layout.fragment_today_menu, container, false);
+        activity = (MainActivity) getActivity();
+        txtTitle = (TextView) mView.findViewById(R.id.txt_title_menu);
+        imageMenu = (ImageView) mView.findViewById(R.id.image_today_menu);
         try {
-            Mon mon=getArguments().getParcelable(Constants.DATA);
-            if(mon!=null){
+            mon = getArguments().getParcelable(Constants.DATA);
+            if (mon != null) {
                 txtTitle.setText(mon.getTitle());
-                Utils.loadImage(activity,imageMenu,mon.getImage());
+                Utils.loadImage(activity, imageMenu, mon.getImage());
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+        mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(activity, DetailMonActivity.class);
+                intent.putExtra(Constants.DATA,mon);
+//                startActivity(intent);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    startActivity(intent,
+                            ActivityOptions.makeSceneTransitionAnimation(activity).toBundle());
+                }else{
+                    startActivity(intent);
+
+                }
+
+            }
+        });
         return mView;
     }
+
 }
